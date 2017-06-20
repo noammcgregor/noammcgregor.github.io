366ef8ce46172c635ed66a7dbf9ef026a4ffd2da
noammcgregor <nmcgregor96@gmail.com>
2017-06-20 13:29:57 -0400

    .

--- scripts/email.js
+++ scripts/email.js
@@ -4,8 +4,8 @@ function golf(n) {
 	clearAll();
 	
 	var golfs = document.getElementsByClassName("golf");
-	var t1="mailto:perspectiveaerialmedia@gmail.com?subject=Golf%20Course%Aerial%20Media%20Inquiry&body=Hi,%0AI%20am%20interested%20in%20";
-	var t2="Here%20is%20my%20following%20information:%0A%0AName:%0A%0AFilming%20Location:%0A%0ARequired%20Completion%20Date:%0A%0AAdditional%20Comments/Questions:%0A%0A%0A***Thank%20you%20for%20your%20time.%20We%20will%20be%20in%20touch%20shortly%20with%20a%20quote%20and%20expected%20time%20Frame***";
+	var t1="mailto:perspectiveaerialmedia@gmail.com?subject=Golf%20Course%20Aerial%20Media%20Inquiry&body=Hi,%0A%0AI%20am%20interested%20in%20";
+	var t2="Here%20is%20my%20following%20information:%0A%0A%0AName:%0A%0ACourse%20Name:%0A%0AFilming%20Location:%0A%0ARequired%20Completion%20Date:%0A%0AAdditional%20Comments/Questions:%0A%0A%0A***Thank%20you%20for%20your%20time.%20We%20will%20be%20in%20touch%20shortly%20with%20a%20quote%20and%20expected%20time%20Frame***";
 	t1+=packages[n];
 	document.getElementById("golfbutton").setAttribute("href", t1+t2);
 	golfs[n].className += " w3-teal w3-hover-teal";
@@ -16,6 +16,7 @@ function estate(n) {
 	
 	var estates = document.getElementsByClassName("estate");
 	var t1="mailto:perspectiveaerialmedia@gmail.com?subject=Real%20Estate%Aerial%20Media%20Inquiry&body=Hi,%0AI%20am%20interested%20in%20";
+	var t2="Here%20is%20my%20following%20information:%0A%0A%0AFirst%20Name:%0A%0ALast%20Name:%0A%0AFilming%20Location:%0A%0ARequired%20Completion%20Date:%0A%0AAdditional%20Comments/Questions:%0A%0A%0A***Thank%20you%20for%20your%20time.%20We%20will%20be%20in%20touch%20shortly%20with%20a%20quote%20and%20expected%20time%20Frame***";
 	t1+=packages[n];
 	document.getElementById("estatebutton").setAttribute("href", t1);
 	
@@ -27,6 +28,7 @@ function specialevent(n) {
 	
 	var events = document.getElementsByClassName("event");
 	var t1="mailto:perspectiveaerialmedia@gmail.com?subject=Special%20Event%Aerial%20Media%20Inquiry&body=Hi,%0AI%20am%20interested%20in%20";
+	var t2="Here%20is%20my%20following%20information:%0A%0A%0AFirst%20Name:%0A%0ALast%20Name:%0A%0AFilming%20Location:%0A%0ARequired%20Completion%20Date:%0A%0AAdditional%20Comments/Questions:%0A%0A%0A***Thank%20you%20for%20your%20time.%20We%20will%20be%20in%20touch%20shortly%20with%20a%20quote%20and%20expected%20time%20Frame***";
 	t1+=packages[n];
 	document.getElementById("eventbutton").setAttribute("href", t1);
 	
@@ -37,6 +39,7 @@ function other(n) {
 	clearAll();
 	var others = document.getElementsByClassName("other");
 	var t1="mailto:perspectiveaerialmedia@gmail.com?subject=Other%Aerial%20Media%20Inquiry&body=Hi,%0AI%20am%20interested%20in%20";
+	var t2="Here%20is%20my%20following%20information:%0A%0A%0AFirst%20Name:%0A%0ALast%20Name:%0A%0AFilming%20Location:%0A%0ARequired%20Completion%20Date:%0A%0AAdditional%20Comments/Questions:%0A%0A%0A***Thank%20you%20for%20your%20time.%20We%20will%20be%20in%20touch%20shortly%20with%20a%20quote%20and%20expected%20time%20Frame***";
 	t1+=packages[n];
 	document.getElementById("otherbutton").setAttribute("href", t1);
 	
