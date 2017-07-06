3da4edbd286e0f600cc613a50fdd5532128802e5
noammcgregor <nmcgregor96@gmail.com>
2017-07-06 13:34:40 -0400

    .

--- scripts/slideShow.js
+++ scripts/slideShow.js
@@ -28,5 +28,5 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-white";
-  setTimeout(autoSlide, 9000);
+  setTimeout(autoSlide, 5000);
 }
